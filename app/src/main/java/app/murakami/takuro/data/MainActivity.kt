3b5fedb0a9b59dbaca52bc08924bab11f86e7c02
takuro <67560179+nuaseTakuro@users.noreply.github.com>
2021-05-27 04:37:45 +0900

    データ保存完成

---
+++ app/src/main/java/app/murakami/takuro/data/MainActivity.kt
@@ -0,0 +1,33 @@
+package app.murakami.takuro.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataStore: SharedPreferences = getSharedPreferences("DataStore", Context.MODE_PRIVATE)
+
+        saveButton.setOnClickListener {
+
+            val stringText = editText.text.toString()
+            saveText.text = stringText
+
+            val editor = dataStore.edit()
+            editor.putString("Input",stringText)
+
+            editor.apply()
+
+        }
+
+        readButoon.setOnClickListener {
+
+            val str = dataStore.getString("Input","NoData")
+        }
+    }
+}
